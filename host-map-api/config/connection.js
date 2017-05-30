4ecdafa697c0c169c1ff9bf5cb318e3284a1909f
Cassio Alves <cassio.alves09@gmail.com>
2017-05-30 10:52:37 -0300

    commit

--- host-map-api/config/connection.js
+++ host-map-api/config/connection.js
@@ -21,7 +21,7 @@ function getUrlConnection() {
     return 'mongodb://'
         + db.usuario + ':'
         + db.senha
-        +'@ds155841.mlab.com:55841/mean-delta';
+        +'@ds155841.mlab.com:55841/meandelta';
 }
 
 module.exports = MongoDBConnection();
